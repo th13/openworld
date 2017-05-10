d89f17fa920344d06a6eff23a7bd0c0991b7c37e
Trevor Helms <dev@trevorhelms.com>
2017-05-10 00:11:17 -0400

    Add boilerplate and appropriate HTTP status codes to methods

--- services/api/src/api.py
+++ services/api/src/api.py
@@ -1,10 +1,35 @@
+import os
 from flask import Flask
+from functools import wraps
 
-# TODO: Move these to env variables.
-API_PORT = 5000
-API_DEBUG = True
+config = {
+    'host': os.environ['API_HOST'],
+    'port': os.environ['API_PORT'],
+    'debug': True if os.environ['API_DEBUG'] == 1 else False
+}
+
 
 api = Flask(__name__)
+
+
+def not_implemented(route_fn):
+    """Flask route decorator for routes that haven't been implemented yet."""
+    @wraps(route_fn)
+    def decorated(*args, **kwargs):
+        return 'This functionality has not been implemented yet.', 501
+    return decorated
+
+### TODO: Implement all unimplemented methods.
+
+@api.route('/')
+def root():
+    """Everyone loves easter eggs."""
+    return 'http://www.youtube.com/watch?v=e69-GO4bYLM', 418
+
+@api.route('/api')
+@not_implemented
+def api_home():
+    pass
 
 @api.route('/login', methods=['POST'])
 @not_implemented
@@ -20,9 +45,8 @@ def logout():
 def page_not_found(error):
     pass
 
+
 if __name__ == '__main__':
-    api.run('0.0.0.0', port=API_PORT, debug=API_DEBUG)
-
-
-def not_implemented(route_fn):
-    return 'This functionality has not been implemented yet', 501
+    api.run(host=config['host'], 
+            port=config['port'], 
+            debug=config['debug'])
