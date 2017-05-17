7e309b583207bca338f62c25edbe1a2d1063b041
Trevor Helms <dev@trevorhelms.com>
2017-05-17 16:54:07 -0400

    UI design

--- services/api/api/ws/api.py
+++ services/api/api/ws/api.py
@@ -1,11 +1,35 @@
+## api.py
+##
+## Contains the SocketIO event-based API for OpenWorld.
+
 from flask_socketio import send, emit
 from server import socketio
+from functools import wraps
+
+auth_tokens = { 'abcdefg', 'bigisland' }
+
+def authenticate(fn):
+    """Decorator to require authentication for an event handler."""
+    @wraps(fn)
+    def wrapper(data):
+        if not 'auth' in data or not data['auth'] in auth_tokens:
+            emit('authentication_failed')
+            return
+        fn(data)
+    return wrapper
+
 
 @socketio.on('connect')
 def on_connect():
-    pass
+    """Called when a client has connected to the server.
 
-@socketio.on('client_connected')
-def on_client_connected(data):
-    print(data)
-    emit('yes', data)
+    Emits an authentication_requested event to tell the client they need to
+    authenticate.
+    """
+    emit('authentication_requested')
+
+
+@socketio.on('get_some_data')
+@authenticate
+def on_get_some_data(data):
+    emit('here_is_data', { 'data': 'hi' })
